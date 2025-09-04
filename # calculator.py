dc2eb302757e05c28544b6a6f02ea4ff7cbb6971
sdangelo-ai <sdangelo@mail.immaculata.edu>
2025-09-04 09:23:37 -0700

    1st commit

---
+++ # calculator.py
@@ -0,0 +1,38 @@
+# calculator 
+def sum (a, b): # function to add two numbers
+          return (a+b)
+a = int (input ('Enter 1st number: '))
+#accept an intger input from user
+b = int (input ('Enter 2nd numbers: '))
+#print the variables and their sum
+print(f'Sum of {a} and {b} is {sum(a, b)}') 
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x ,y): #multiplication function
+        return x + y 
+def divide(x ,y): #division function
+     if y !=0:
+         return x / y
+     else:
+        return "Error! Not possible to divide by zero,"
+print("Select operation:")
+print("1. Add")
+print("2, Subtract")
+print("3, Multiply")
+print("4, Divide")
+choice =input("Enter first number:")
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+if choice == "1":
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif choice == '2':
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"{num1} * {num2} = {multiply (num1, num2)}")
+elif choice == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+     ("Invalid input") 
+    
